b501507a03cb880307dac1ef131b25014e41a336
kostandinangjellari <kostandinangjellari@gmail.com>
2017-02-07 12:39:31 +0100

    Changed configuration using .env file.

--- app/config/index.js
+++ app/config/index.js
@@ -1,40 +1,40 @@
 'use strict';
 
+require('dotenv').config();
 const _ = require('lodash');
 const Routes = require('./routes');
 
 const API_URL = '/api/v1/';
-const NODE_ENV = process.env.NODE_ENV || 'dev';
 const MILLIS = Math.floor(Date.now() / 1000);
 
+const CONFIG_KEYS = [
+    'HOST',
+    'PORT',
+    'APP_SECRET',
+    'DB_STRING',
+    'FB_CLIENT_ID',
+    'FB_CLIENT_SECRET',
+    'LOG_PATH',
+];
+
 let CommonConfig = {
-    APP_SECRET: process.env.APP_SECRET || 'f1e878c8-e9ad-11e6-bf0e-fe55135034f3',
     Routes: Routes(API_URL)
 };
 
+CONFIG_KEYS.forEach(key => {
+    CommonConfig[key] = process.env[key];
+});
+
 const EnvConfig = {
-    dev: {
-        HOST: 'localhost',
-        PORT: 3000,
-        LOG_PATH: 'logs/',
-        FB_CLIENT_ID: '1860791764203794',
-        FB_CLIENT_SECRET: '4cec0649259475b79dfe4ed66cf5103a',
-        DB_STRING: 'postgres://ttfu:ttfu@localhost:5432/ttfu?ssl=false',
+    development: {
         TOKEN_EXPIRATION_TIME: MILLIS + (60 * 60) //1Hour
     },
-    prod: {
-        HOST: 'localhost',
-        PORT: 8080,
-        LOG_PATH: '/usr/logs/',
-        FB_CLIENT_ID: '1860791764203794',
-        FB_CLIENT_SECRET: '4cec0649259475b79dfe4ed66cf5103a',
-        DB_STRING: '',
-        API_URL: '/api/v1/',
+    production: {
         TOKEN_EXPIRATION_TIME: MILLIS + (24 * 60 * 60) //24Hour
     }
 };
 
-let Config = EnvConfig[NODE_ENV] || {};
+let Config = EnvConfig[process.env.ENVIRONMENT] || {};
 _.extend(Config, CommonConfig);
 
 module.exports = Config;
